37fde9acff626ffbee8455f50b66db5927fa6445
pilauPro <leiningem001@hawaii.rr.com>
2015-07-07 13:18:41 -1000

    css keyframes animation
    Playing around with animating elements in different ways. This version
    uses css keyframe animation, totally bypassing jQuery.

--- js/jq.js
+++ js/jq.js
@@ -1,8 +1,47 @@
 $(document).ready(function(){
-	$('.80s').on('click', function() {
-		price = $(this).find(".title").data('price');
-		$(this).find(".get-price").after("<button class = 'show-price'>$" + price + "</button>")
-		//$(this).after("<button class = 'show-price'>$" + price + "</button>")
-		$(this).find(".get-price").remove();
+
+	$('.movie-wrap').on('click', 'button', function() {
+		price = $(this).siblings(".title").data('price');
+		$(this).after("<button class = 'show-price'>$" + price + "</button>")
+		$(this).remove();
+	});
+
+	// $('.movie-wrap').mouseenter(function() {
+	// 	$(this).css('background-color', 'yellow');
+	// });
+	// $('.movie-wrap').mouseleave(function() {
+	// 	$(this).css('background-color', 'white');
+
+	// });
+
+	$('.filter-row').on('click', 'button', function() {
+
+		var filter = $(this).val();
+		var showMovies = $('.movies').find(filter).find('.movie-wrap');
+		var hideMovies = $('.movies').find('li').not(filter).find('.movie-wrap');
+
+		// highlight the filter selected movie-wraps. If none filter is
+		// selected, showMovies will be empty and hideMovies, which is all
+		// movies that showMovies is not, will be all movies. With all movies
+		// contained in hideMovies, the if statement will leave them alone if
+		// they don't have color, or add the uncoloring class if they do.
+
+		if(showMovies.hasClass('uncoloring')){
+			showMovies.removeClass('uncoloring');
+		}
+
+		showMovies.addClass('coloring');
+
+		if(hideMovies.hasClass('coloring')){
+			hideMovies.addClass('uncoloring');
+		}
+
+		// hideMovies.toggleClass('uncoloring');
+		// hideMovies.hide();
+		// showMovies.show();	
+
+		// $('.highlighted').removeClass('highlighted');
+		// showMovies.addClass('highlighted');
+
 	});
 });
